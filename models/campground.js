e8900697a5f65cd8c1cdd4c147ffebfec1b12ca8
Ninja <cuansenputra@gmail.com>
2023-03-09 13:38:36 +0700

    Design review

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require('./review')
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -12,5 +13,15 @@ const CampgroundSchema = new Schema({
         ref: 'Review'
     }]
 });
+
+CampgroundSchema.post('findOneAndDelete', async (doc) => {
+    if(doc){
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Campground', CampgroundSchema)
