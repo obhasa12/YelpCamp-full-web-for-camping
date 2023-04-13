b192d398c67d0fbe219f064dfbb49d485956cdc8
Ninja <cuansenputra@gmail.com>
2023-04-13 16:34:25 +0700

    add authorization to user

--- routes/user.js
+++ routes/user.js
@@ -28,24 +28,12 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', passport.authenticate('local', {failureFlash : true, failureRedirect : '/login'}), (req, res) => {
     req.flash('success', 'welcome back!');
     const redirectUrl = req.session.returnTo || '/campgrounds';
-    console.log(redirectUrl)
     delete req.session.returnTo;
     res.redirect(redirectUrl);
 })
-
-// router.post('/login', passport.authenticate('local', {failureFlash : true, failureRedirect : '/login'}) , (req, res) => {
-//     req.flash('success', "welcome back!");
-//     res.redirect('/campgrounds')
-// })
-
-// router.get('/logout', (req, res) => {
-//     req.logout();
-//     req.flash('success', "Goodbye!");
-//     res.redirect('/campgrounds');
-// })
 
 router.get('/logout', (req, res) => {
     req.logout(() => {
--- seeds/index.js
+++ seeds/index.js
@@ -21,6 +21,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '642e72a497c9bbb3dc99131a', 
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
