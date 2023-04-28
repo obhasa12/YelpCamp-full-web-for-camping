8ea404e557ea70f2dace6c2db91fc54f062e1a30
Ninja <cuansenputra@gmail.com>
2023-04-28 17:04:30 +0700

    adding upload image feature

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds')
+const multer = require('multer');
+const { storage } = require('../cloudinary')
+const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, 
-        validateCampground, 
-        catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn, 
+    //     validateCampground, 
+    //     catchAsync(campgrounds.createCampground));
+    .post(upload.array('image'), (req, res, next) => {
+        console.log(req.body, req.files);
+        res.send("IT WORKS")
+    })
     
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
