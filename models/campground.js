78354df3c5a57f73f0ac33895017a2ec1f35c1e4
Ninja <cuansenputra@gmail.com>
2023-05-30 16:43:26 +0700

    adding pop up and editing style in cluster map

--- models/campground.js
+++ models/campground.js
@@ -11,6 +11,8 @@ ImageSchema.virtual('thumbnail').get(function(){
     return this.url.replace('/upload', '/upload/w_200')
 })
 
+const option = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
     title: String,
     image: [ ImageSchema ],
@@ -38,6 +40,12 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+},option);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0 , 20)}</p>`
 });
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
